0843d25f8cc5e4cc89645647fbc2cab226ef8645
ChrisRossRokita <88979453+ChrisRossRokita@users.noreply.github.com>
2021-12-08 04:08:42 -0500

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { FcAssistant } from "react-icons/fc";
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink
+} from './NavbarElements';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink to="/home">
+                <h1><FcAssistant /> Margaret C Rokita CPA MST</h1>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to='/about' activeStyle>
+            About
+          </NavLink>
+          <NavLink to='/services' activeStyle>
+            Services
+          </NavLink>
+          <NavLink to='/contact-us' activeStyle>
+            Contact Us
+          </NavLink>
+          <NavLink to='/signup' activeStyle>
+            Sign Up
+          </NavLink>
+          {/* Second Nav */}
+          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
